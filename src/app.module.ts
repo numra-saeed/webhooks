6435a716aa5f954152dd6b8a79c23b0204aa10df
numra-saeed <numrasaeed8@gmail.com>
2025-08-27 22:57:40 +0200

    modifications in root app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payment-webhooks/payments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Invoices } from './payment-webhooks/entities/invoice.entity';
+import { PaymentEvents } from './payment-webhooks/entities/payment-event.entity';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [
+    PaymentsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nestuser',
+      password: 'root',
+      database: 'nestdb',
+      synchronize: true,
+      entities: [Invoices, PaymentEvents]
+    }),
+  ],
   controllers: [],
   providers: [],
 })
